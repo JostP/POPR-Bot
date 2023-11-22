0006ae35414c50de394323b44e073c501515268b
JostP <47271658+JostP@users.noreply.github.com>
2023-11-22 17:39:36 +0100

    Izboljšaj hitrost prijavljanja na aktivnost

--- PoprBot.py
+++ PoprBot.py
@@ -8,42 +8,50 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-username = "email"		# Username za POPR npr. jp1530@student.uni-lj.si
-password = "geslo"
+username = "username"       # Username za POPR npr. "ab1234@student.uni-lj.si"
+password = "password"
 
 finished = False
-dogodek = input("Dogodek: ")
-if dogodek == "hiit" or dogodek == "Hiit":		# Če maš več gumbov bo pri "stevilka_gumba_od_spodaj == 1" pritisnl spodnji gumb, pri "2" predzadnjega itd.
-    stevilka_gumba_od_spodaj = 2
-elif dogodek == "fitnes" or dogodek == "Fitnes":
-    stevilka_gumba_od_spodaj = 1
-elif dogodek == "plavanje" or dogodek == "Plavanje":
-    stevilka_gumba_od_spodaj = 1
-else:
-    print("Neveljaven dogodek")
+
+
+try:
+    dogodek = input("Termin: ")   # Vnesi številko termina. Zadnji termin ima številko 1, predzadnji 2... Če je samo en termin vnesi število 1
+    button_number = int(dogodek)
+
+except ValueError:
+    print("Neveljaven vnos. Vnesi celo stevilo od 1 do 10")
     exit()
 
 
-koda = input("Koda dogodka: ")		# Vsak dogodek ma svojo kodo, to so zadnje štiri številke v URL-ju, npr https://popr.uni-lj.si/.../event/7897     --> 7897
+if not (1 <= button_number <= 10):
+    print("Neveljaven vnos. Vnesi celo stevilo od 1 do 10")
+    exit()
+
+
+koda = input("Koda dogodka: ")      # Vsak dogodek ma svojo kodo, to so zadnje štiri številke v URL-ju, npr https://popr.uni-lj.si/.../event/7897     --> 7897
 
 hour = time.localtime(time.time()).tm_hour
 minute = time.localtime(time.time()).tm_min
 sec = time.localtime(time.time()).tm_sec
-print("Trenuten čas: " + str(hour) + ":" + str(minute))
-print("Čakanje na uro začetka prijavljanja: 06:01")
+
+formatted_time = "{:02d}:{:02d}:{:02d}".format(hour, minute, sec)
+
+print("---------------------------------------------------")
+print("Trenuten čas: " + formatted_time)
+print("Čakanje na začetek prijavljanja...")
+print("Lahko noč:)")
 
 while not finished:
     hour = time.localtime(time.time()).tm_hour
     minute = time.localtime(time.time()).tm_min
     sec = time.localtime(time.time()).tm_sec
-    
-    if hour == 6 and minute == 1: # Začel se bo prijavljat ob 06:01
-        print("Začetek prijavljanja ob", str(hour) + ":" + str(minute))
+
+    if hour == 5 and minute == 59: # V sistem POPR se bo prijavil minuto prej. Na dogodek se bo prijavil ob 6:00:00
         options = FirefoxOptions()
-        options.add_argument("--headless")
+        options.add_argument("--headless") # Program ne bo odprl firefox-a ampak bo firefox tekel v ozadju
         driver = webdriver.Firefox(options=options)
         driver.get('https://popr.uni-lj.si//student/home.html')
-        
+
         try:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'username')))
         except TimeoutException:
@@ -52,26 +60,47 @@ while not finished:
 
         uporabnik = driver.find_element("id", 'username')
         uporabnik.send_keys(username)
-        
+
         geslo = driver.find_element("id", 'user_pass')
         geslo.send_keys(password)
-        
+
         poslji = driver.find_element('id', 'wp-submit')
         poslji.click()
-        
+
         try:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'yesbutton')))
         except TimeoutException:
             print("Loading took too much time!")
             exit()
-         
-        bt = driver.find_element('id', 'yesbutton')
-        text = bt.text
-        bt.click()
-        print("Prijavljen v POPR")
-        time.sleep(5)
-        driver.get('https://popr.uni-lj.si/student/svc/events.html#/user/event/' + str(koda))
+
+        button = driver.find_element('id', 'yesbutton')
+        text = button.text
+        button.click()
         
+        hour = time.localtime(time.time()).tm_hour
+        minute = time.localtime(time.time()).tm_min
+        sec = time.localtime(time.time()).tm_sec
+
+        formatted_time = "{:02d}:{:02d}:{:02d}".format(hour, minute, sec)
+        
+        print("Uspešno prijavljen v POPR ob " + formatted_time + "!")
+        print("Čakanje na 6:00:00 za prijavo na dogodek...")
+
+        while not (hour == 6 and minute == 0):              # Počakaj da bo ura 6:00:00
+            
+            hour = time.localtime(time.time()).tm_hour
+            minute = time.localtime(time.time()).tm_min
+            sec = time.localtime(time.time()).tm_sec
+            formatted_time = "{:02d}:{:02d}:{:02d}".format(hour, minute, sec)
+            
+            time.sleep(0.1)   # Počakaj 0,1 sekunde vsak krog, da se ne vrti na polno
+            print(formatted_time)
+
+        print("Čas: " + formatted_time)
+        print("Začetek prijavljanja v aktivnost...")
+        
+        driver.get('https://popr.uni-lj.si/student/svc/events.html#/user/event/' + str(koda))
+
         try:
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm')))
         except TimeoutException:
@@ -79,45 +108,50 @@ while not finished:
             exit()
 
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        bt = driver.find_elements(By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm')
-        time.sleep(1)
-        joinButton = bt[len(bt) - stevilka_gumba_od_spodaj]
+        button= driver.find_elements(By.CSS_SELECTOR, 'button.btn.btn-primary.btn-sm')
         
+        time.sleep(0.3)
+        joinButton = button[len(button) - button_number]
+
         if str(joinButton.text) == "Book":
             joinButton.click()
         else:
             print("Gumb ni bil najden: ", joinButton.text)
             exit()
-            
-        time.sleep(1)
-        bt = driver.find_elements(By.CSS_SELECTOR, 'button.btn.btn-primary')
-        
-        if str(bt[len(bt)-1].text) == "Yes":
-            bt[len(bt)-1].click()
+
+        button= driver.find_elements(By.CSS_SELECTOR, 'button.btn.btn-primary')
+
+        if str(button[len(button)-1].text) == "Yes":
+            button[len(button)-1].click()
 
         else:
-            print("Gumb ni bil najden: ", bt[len(bt)-1].text)
+            print("Gumb ni bil najden: ", button[len(button)-1].text)
 
         try:
-			# Pri enih dogodkih moraš napisat kje si izvedu za ta dogodek, ponavad na nekaterih slabo obiskanih delavnicah iz FDVja, no offence:),
-			# pa pol rabs se obkljukat en checkbox, pri športnih aktivnostih tega ni, zato gre program sam naprej če ne najde checkboxa.
+            # Pri enih dogodkih moraš napisat kje si izvedu za ta dogodek, ponavad na nekaterih slabo obiskanih delavnicah iz FDVja, no offence:),
+            # pa pol rabs se obkljukat en checkbox, pri športnih aktivnostih tega ni, zato gre program sam naprej če ne najde checkboxa.
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@type='checkbox']")))
-            bt = driver.find_element(By.XPATH, "//input[@type='checkbox']")
-            bt.click()
-            
+            button= driver.find_element(By.XPATH, "//input[@type='checkbox']")
+            button.click()
+
         except TimeoutException:
             print("Neuspešno obkljukan checkbox oziroma ga sploh ni")
 
         try:
-            bt = driver.find_element(By.XPATH, "//button[@type='submit']")
-            bt.click()
-            
+            button= driver.find_element(By.XPATH, "//button[@type='submit']")
+            button.click()
+
         except:
             print("Neuspešna prijava")
             exit()
 
-        print("Uspešno prijavljen na dogodek:)!")
+        hour = time.localtime(time.time()).tm_hour
+        minute = time.localtime(time.time()).tm_min
+        sec = time.localtime(time.time()).tm_sec
+        formatted_time = "{:02d}:{:02d}:{:02d}".format(hour, minute, sec)
+
+        print("Uspešno prijavljen na dogodek :)")
+        print("Prijavljen si bil ob: " + formatted_time)
+
         finished = True
-    time.sleep(60)
-
-
+    time.sleep(10)  # Počaka 10 sekund, da se while zanka ne porabi preveč računske moči
